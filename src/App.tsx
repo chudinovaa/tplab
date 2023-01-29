89cde2167ad2b5aaf537987064b422667469bf38
Anton <Nutsway@e1.ru>
2023-01-29 20:00:24 +0100

    fixed bugs with styles in productItem and added favicon

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React, {useEffect} from 'react';
 import {Routes, Route} from 'react-router-dom';
 import ProductsPage from './pages/ProductsPage';
 import ProductItemPage from './pages/ProductItemPage';
-import {useAppDispatch} from './hooks';
+import {useAppDispatch, useAppSelector} from './hooks';
 import {fetchProducts} from './store/productSlice';
 
 function App() {
     const dispatch = useAppDispatch()
-
+    const {list} = useAppSelector(state => state.products)
     useEffect(() => {
-        dispatch(fetchProducts())
+            dispatch(fetchProducts())
     },[])
 
     return (
